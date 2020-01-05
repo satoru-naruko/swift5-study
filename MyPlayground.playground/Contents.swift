7bcca61b75983865f9bb914927fcbbcf8ebd1f70
satoru-naruko <naruko505@gmail.com>
2020-01-06 00:33:11 +0900

    switch whileの構文を追加した

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,52 @@
 import UIKit
 
+//xcode short cut
+// shift + cmd + Enter -> executePlayground
+
 var str = "Hello, playground"
+var age : Int = 10
+//age = 10
+print(age)
+
+// if
+if age <= 20 {
+    print("if age :", age)
+//} else if case 21 ... 30 = age {
+// ... means 21 <= age <= 30
+} else if case 21 ..< 30 = age {
+    print("else if age :", age)
+} else {
+    print("else root age :", age)
+}
+
+// for and where
+for i in 1 ..< 5 where i % 2 == 0 {
+    print (" for loop ", i)
+}
+
+// swtch
+switch age {
+    case 30:
+    print("adult")
+    print("boy")
+    case 10:
+    print(age,"years old")
+    print("next year 11 years old")
+    fallthrough
+default:
+    print("default age :")
+}
+
+loop: while true {
+    for i in 1..<7 {
+        if i == 4 {
+            // out while loop exit
+            // break labelname
+            break loop
+        }
+        print("loop i:", i);
+    }
+    print("while loop =======")
+}
+
+print("while loop exit")
